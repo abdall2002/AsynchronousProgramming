b483accb45baaff3c6fce00db085254d82590aeb
unknown <abdallahaliahmed517@gmail.com>
2023-08-21 14:53:03 +0300

    AAA

---
+++ CARaceCondition/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using CARaceCondition;
+
+var wallet = new Wallet("Issam", 100);
+
+//wallet.Debit(40);
+//wallet.Debit(30); // 10
+
+
+var t1 = new Thread(() => wallet.Debit(40));
+var t2 = new Thread(() => wallet.Debit(30));
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+Console.WriteLine(wallet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CARaceCondition/Wallet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace CARaceCondition
+{
+    class Wallet
+    {
+        private readonly object bitcoinsLock = new object();
+        public Wallet(string name, int bitcoins)
+        {
+            Name = name;
+            Bitcoins = bitcoins;
+        }
+
+        public string Name { get; private set; }
+        public int Bitcoins { get; private set; }
+
+
+        public void Debit(int amount)
+        {
+            lock (bitcoinsLock)
+            {
+                if (Bitcoins >= amount)
+                {
+                    Thread.Sleep(1000);
+
+                    Bitcoins -= amount;
+                }
+            }
+        }
+
+        public void Credit(int amount)
+        {
+            Thread.Sleep(1000);
+            Bitcoins += amount;
+        }
+
+
+        public override string ToString()
+        {
+            return $"[{Name} -> {Bitcoins} Bitcoins]";
+        }
+    }
+}
+    
+
