f0b0f7bbe4c72c2fa3fa8a0da875c6f42f257e8b
unknown <abdallahaliahmed517@gmail.com>
2023-08-21 12:56:18 +0300

    AAA

---
+++ CASequential/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using CASequential;
+
+var wallet = new Wallet("Issam", 80);
+
+wallet.RunRandomTransactions();
+Console.WriteLine("----------------");
+Console.WriteLine($"{wallet}\n");
+
+wallet.RunRandomTransactions();
+Console.WriteLine("----------------");
+Console.WriteLine($"{wallet}\n");
+
+
+
+
+
+
+
---
+++ CASequential/Wallet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace CASequential
+{
+    class Wallet
+    {
+        public Wallet(string name, int bitcoins)
+        {
+            Name = name;
+            Bitcoins = bitcoins;
+        }
+
+        public string Name { get; set; }
+        public int Bitcoins { get; set; }
+
+        public void Debit(int amount)
+        {
+            Bitcoins -= amount;
+        }
+
+        public void Credit(int amount)
+        {
+            Bitcoins += amount;
+        }
+
+        public void RunRandomTransactions()
+        {
+            int[] amounts = { 10, 20, 30, -20, 10, -10, 30, -10, 40, -20 }; // 80
+
+            foreach (var amount in amounts)
+            {
+                var absValue = Math.Abs(amount);
+                if (amount < 0)
+                    Debit(absValue);
+                else
+                    Credit(absValue);
+                Console.WriteLine($"[Thread: {Thread.CurrentThread.ManagedThreadId}" +
+                    $", Processor Id: {Thread.GetCurrentProcessorId()}] {amount}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{Name} -> {Bitcoins} Bitcoins]";
+        }
+    }
+}
