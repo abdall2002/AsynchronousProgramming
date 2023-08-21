bd47fafd26d78f778a42b7967a1ffd4bff9a07ff
unknown <abdallahaliahmed517@gmail.com>
2023-08-21 14:17:01 +0300

    AAA

--- CAMultithreading/Program.cs
+++ CAMultithreading/Program.cs
@@ -6,20 +6,20 @@ using CAMultithreading;
 
 Thread.CurrentThread.Name = "Main Thread";
 Console.WriteLine(Thread.CurrentThread.Name);
-Console.WriteLine($"Background Thread: {Thread.CurrentThread.IsBackground}");
+//Console.WriteLine($"Background Thread: {Thread.CurrentThread.IsBackground}");
 var wallet = new Wallet("Issam", 80);
 
 Thread t1 = new Thread(wallet.RunRandomTransactions);
 t1.Name = "T1";
-Console.WriteLine($"T1 Background Thread: {t1.IsBackground}");
-//Console.WriteLine($"after declaration {t1.Name} state is: {t1.ThreadState}");
+//Console.WriteLine($"T1 Background Thread: {t1.IsBackground}");
+Console.WriteLine($"after declaration {t1.Name} state is: {t1.ThreadState}");
 
-//t1.Start();
-//Console.WriteLine($"after start() {t1.Name} state is: {t1.ThreadState}");
+t1.Start();
+Console.WriteLine($"after start() {t1.Name} state is: {t1.ThreadState}");
 //t1.Join();
 
-//Thread t2 = new Thread(new ThreadStart(wallet.RunRandomTransactions));
-//t2.Name = "T2";
-//t2.Start();
+Thread t2 = new Thread(new ThreadStart(wallet.RunRandomTransactions));  // T1,T2 Working in parallel;
+t2.Name = "T2";
+t2.Start();
 
-//Console.WriteLine($"after start {t1.Name} state is: {t1.ThreadState}");
+Console.WriteLine($"after start {t1.Name} state is: {t1.ThreadState}");
