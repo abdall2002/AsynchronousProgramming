b8e5092b03ef6ff474fbc21603ffa3dfe8bf1f62
unknown <abdallahaliahmed517@gmail.com>
2023-08-21 12:15:39 +0300

    AAA

---
+++ CAProcessAndThread/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+
+Console.WriteLine($"Process Id: {Process.GetCurrentProcess().Id}");
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+Console.WriteLine($"Processor Id: {Thread.GetCurrentProcessorId()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
