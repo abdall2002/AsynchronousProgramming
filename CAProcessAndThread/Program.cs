c57d56719ccc2723a5156e94b861bddb73602e1b
unknown <abdallahaliahmed517@gmail.com>
2023-08-21 12:20:15 +0300

    AAA

--- CAProcessAndThread/Program.cs
+++ CAProcessAndThread/Program.cs
@@ -4,9 +4,9 @@ using System.Threading;
 using System.Diagnostics;
 
 
-Console.WriteLine($"Process Id: {Process.GetCurrentProcess().Id}");
-Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
-Console.WriteLine($"Processor Id: {Thread.GetCurrentProcessorId()}");
+Console.WriteLine($"Process Id: {Process.GetCurrentProcess().Id}");       // 9865 any number
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");  // 1
+Console.WriteLine($"Processor Id: {Thread.GetCurrentProcessorId()}");     // 6 or 2 or 8
 
 
 
