93f2e7e88f10e221472a22ab6d3cf1c74adf6963
unknown <abdallahaliahmed517@gmail.com>
2023-08-22 12:13:32 +0300

    AAA

--- CAThreadPool/Program.cs
+++ CAThreadPool/Program.cs
@@ -3,8 +3,8 @@
 
 using System;
 
- 
-  Print();
+//Task.Run(Print); 
+Print();
 async Task Print()
 {
     Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}, Thread Name: {Thread.CurrentThread.Name}");
@@ -14,5 +14,16 @@ async Task Print()
     {
         Console.WriteLine($"Cycle {i + 1}");
     }
-    
+
 }
+
+//void Print(object state)
+//{
+//    Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}, Thread Name: {Thread.CurrentThread.Name}");
+//    Console.WriteLine($"Is Pooled thread: {Thread.CurrentThread.IsThreadPoolThread}");
+//    Console.WriteLine($"Background: {Thread.CurrentThread.IsBackground}");
+//    for (int i = 0; i < 10; i++)
+//    {
+//        Console.WriteLine($"Cycle {i + 1}");
+//    }
+//}
