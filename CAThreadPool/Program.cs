a1c3b7850435244ff1940e349bbd9946168fabeb
unknown <abdallahaliahmed517@gmail.com>
2023-08-22 12:12:48 +0300

    AAA

---
+++ CAThreadPool/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+ 
+  Print();
+async Task Print()
+{
+    Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}, Thread Name: {Thread.CurrentThread.Name}");
+    Console.WriteLine($"Is Pooled thread: {Thread.CurrentThread.IsThreadPoolThread}");
+    Console.WriteLine($"Background: {Thread.CurrentThread.IsBackground}");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"Cycle {i + 1}");
+    }
+    
+}
