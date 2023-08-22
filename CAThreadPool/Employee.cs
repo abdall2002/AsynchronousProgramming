bed3bd43b851facf1cfdc4dd083695a7ed086cce
unknown <abdallahaliahmed517@gmail.com>
2023-08-22 12:26:28 +0300

    AAA

--- CAThreadPool/Employee.cs
+++ CAThreadPool/Employee.cs
@@ -8,6 +8,8 @@ namespace CAThreadPool
 {
     public class Employee
     {
-
+        public decimal TotalHours { get; set; }
+        public decimal Rate { get; set; }
+        public decimal TotalSalary { get; set; }
     }
 }
--- CAThreadPool/Program.cs
+++ CAThreadPool/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using CAThreadPool;
 using System;
 
 // -> 1
 //Task.Run(Print);
 // -> 2
 Print();
+
+var employee = new Employee { Rate = 10, TotalHours = 40 };
+
+await Task.Run(CalculateSalary);    // -> $400.00
+void CalculateSalary()
+{
+    var emp = employee as Employee;
+    if (employee is null)
+        return;
+    emp.TotalSalary = emp.TotalHours * emp.Rate;
+    Console.WriteLine(emp.TotalSalary.ToString("C"));
+}
+
+
 async Task Print()
 {
     Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}, Thread Name: {Thread.CurrentThread.Name}");
